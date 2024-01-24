e9088a702113d6730cee4443eba86c0ca9ec7edd
vishalchaurasia10 <1ms21cs138@msrit.edu>
2024-01-24 23:29:45 +0530

    modified the extension and added styling

--- content.js
+++ content.js
@@ -26,4 +26,37 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
         sendResponse({ imagesData: imagesData });
     }
+    if (request.action == 'highlightElements') {
+        highlightElements(request.xpath);
+    } else if (request.action == 'highlightElementsOnHover') {
+        addHoverEffect(request.xpath);
+    }
 });
+
+function highlightElements(xpath) {
+    const elements = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+    let element = elements.iterateNext();
+    console.log(element)
+
+    while (element) {
+        element.style.border = '10px solid red';
+        element = elements.iterateNext();
+    }
+}
+
+function addHoverEffect(xpath) {
+    const elements = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+    let element = elements.iterateNext();
+
+    while (element) {
+        element.addEventListener('mouseover', function () {
+            this.style.border = '10px solid red';
+        });
+
+        element.addEventListener('mouseout', function () {
+            this.style.border = ''; // Remove border on mouseout
+        });
+
+        element = elements.iterateNext();
+    }
+}
--- popup.js
+++ popup.js
@@ -1,15 +1,17 @@
-document.getElementById('getDom').addEventListener('click', function () {
+document.getElementById('getElement').addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'getDom' }, function (response) {
-            console.log(response.dom);
-        });
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'highlightElements', xpath: '//*[@id="top"]/div/center/img' });
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'highlightElements', xpath: '//*[@id="top"]/footer/div[2]/center/img' });
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'highlightElements', xpath: '//*[@id="top"]/main/article/div[2]/center/img' });
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'highlightElements', xpath: '//*[@id="image-popup"]/img' });
     });
 });
 
-document.getElementById('getTable').addEventListener('click', function () {
+document.getElementById('getElementOnHover').addEventListener('click', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'getTable' }, function (response) {
-            console.log(response.imagesData);
-        });
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'highlightElementsOnHover', xpath: '//*[@id="top"]/div/center/img' });
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'highlightElementsOnHover', xpath: '//*[@id="top"]/footer/div[2]/center/img' });
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'highlightElementsOnHover', xpath: '//*[@id="top"]/main/article/div[2]/center/img' });
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'highlightElementsOnHover', xpath: '//*[@id="image-popup"]/img' });
     });
 });
